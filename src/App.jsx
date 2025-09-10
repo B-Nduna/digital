21c833c27b8099363070acfbea264aa0cb5df180
Bongani Nduna <159546536+B-Nduna@users.noreply.github.com>
2025-09-10 20:21:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 
       <section className="profile">
         <div className="image-stack">
-          <img src="/digital/images/profile.webp" className="main-profile" alt="Profile" />
+          <img src="/digital/images/Profile.webp" className="main-profile" alt="Profile" />
         </div>
 
         <p>

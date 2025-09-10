e58b223806efbd925fa960cd4cc6124122460505
B-Nduna <159546536+B-Nduna@users.noreply.github.com>
2025-09-11 00:02:45 +0200

    image adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 
       <section className="profile">
         <div className="image-stack">
-          <img src="/digital/images/Profile.webp" className="main-profile" alt="Profile" />
+          <img src={`${import.meta.env.BASE_URL}images/Profile.webp`} className="main-profile" alt="Profile"/>
         </div>
 
         <p>
